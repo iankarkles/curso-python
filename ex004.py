c4575560ecd5b3ac3d9a3e2b7c65788092a51ac8
Ian <iankkles@gmail.com>
2021-11-25 19:42:13 -0300

    Criação Ex4
    Criação do Ex4, testando as validações de String.

---
+++ ex004.py
@@ -0,0 +1,25 @@
+#Minha tentativa
+
+str = input("Digite algo: ")
+print("O tipo primitivo desse dado é {}".format(type(str)))
+print("Só tem espaços? {}".format(str.isspace()))
+print("Só tem números? {}".format(str.isnumeric()))
+print("Só tem alfabéticos? {}".format(str.isalpha()))
+print("É alfanúmerico? {}".format(str.isalnum()))
+print("Está em maiúsculas? {}".format(str.isupper()))
+print("Está em minuúsculas? {}".format(str.islower()))
+print("Está capitalizada? {}".format(str[0].isupper()))
+
+
+#Código
+ 
+ 
+str = input("Digite algo: ")
+print("O tipo primitivo desse dado é {}".format(type(str)))
+print("Só tem espaços? {}".format(str.isspace()))
+print("Só tem números? {}".format(str.isnumeric()))
+print("Só tem alfabéticos? {}".format(str.isalpha()))
+print("É alfanúmerico? {}".format(str.isalnum()))
+print("Está em maiúsculas? {}".format(str.isupper()))
+print("Está em minuúsculas? {}".format(str.islower()))
+print("Está capitalizada? {}".format(str[0].isupper()))
