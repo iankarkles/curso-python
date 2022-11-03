8011d481ead6d2cd3df7587033af39ecdc2221bb
Ian <iankkles@gmail.com>
2022-11-03 21:56:58 +0000

    Update ex063.py

---
+++ ex063.py
@@ -0,0 +1,12 @@
+print('-'*15)
+print('Sequência de Fibonatti')
+print('-'*15)
+numero = int(input('Quantos termos você quer mostrar? '))
+contador = 0
+f0 = 0
+f1 = 1
+fn = f
+while not contador == numero:
+    print(f"{numero} --> ",end="")
+    numero = numero + fator
+    contador += 1
